c80377643a0c79b20ddaff2e4e85aac6b64c394e
miguel <mallquilm@gmail.com>
2024-11-02 23:53:34 -0500

    cambios con thymeleaf

--- src/main/java/edu/cibertec/cap2/controller/UsuarioController.java
+++ src/main/java/edu/cibertec/cap2/controller/UsuarioController.java
@@ -2,45 +2,31 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package edu.cibertec.cap2;
+package edu.cibertec.cap2.controller;
 
 import edu.cibertec.cap2.model.UsuarioDTO;
 import edu.cibertec.cap2.service.UsuarioService;
+import java.util.Base64;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
+@SessionAttributes("contador")
 public class UsuarioController {
 
     @Autowired
     private UsuarioService usuarioService;
-    
-    @RequestMapping("usuarioCrear")
-    public ModelAndView crearUsuario(){
-        return new ModelAndView("usuarioDatos", "usuarioBean", new UsuarioDTO());
-    }
-    
-    @RequestMapping("usuarioGrabar")
-    public ModelAndView grabarUsuario(@Valid @ModelAttribute("usuarioBean") UsuarioDTO usuario, BindingResult resulta){
-        
-        ModelAndView mv = null;
-        if(resulta.hasErrors()){
-            mv = new ModelAndView("usuario", "usuarioBean", usuario);
-        }else{
-            usuarioService.insertarUsuario(usuario);
-            mv = new ModelAndView("usuarioLista", "lista", usuarioService.getListaUsuarios());
-        }
-        return mv;
-    }
 
     @RequestMapping("loginMostrar")
     public String loginMostrar() {
@@ -48,27 +34,53 @@ public class UsuarioController {
     }
 
     @RequestMapping("loginAccion")
-    public ModelAndView loginAccion(UsuarioDTO usuariovalida) {
+    public ModelAndView loginAccion(UsuarioDTO usuarioValida) {
         ModelAndView mv = null;
-        
-        UsuarioDTO ue = usuarioService.validarLogin(usuariovalida);
+        UsuarioDTO ue = usuarioService.validarLogin(usuarioValida);
         if (ue == null) {
             mv = new ModelAndView("login", "msgError", "Usuario y clave no existen. Vuelva a intentar!");
         } else {
             mv = new ModelAndView("usuarioLista", "lista", usuarioService.getListaUsuarios());
+            mv.addObject("contador", 0);
         }
         return mv;
     }
-    
+
+    @RequestMapping("usuarioCrear")
+    public ModelAndView crearUsuario() {
+        return new ModelAndView("usuarioDatos", "usuarioBean", new UsuarioDTO());
+    }
+
+    @RequestMapping("usuarioGrabar")
+    public ModelAndView grabarUsuario(@Valid @ModelAttribute("usuarioBean") UsuarioDTO usuario, BindingResult resulta, ModelMap modelo) {
+        ModelAndView mv = null;
+        if (resulta.hasErrors()) {
+            mv = new ModelAndView("usuarioDatos", "usuarioBean", usuario);
+        } else {
+            usuarioService.insertarUsuario(usuario);
+            mv = new ModelAndView("usuarioLista", "lista", usuarioService.getListaUsuarios());
+            int contador = (int) modelo.get("contador");
+            contador++;
+            mv.addObject("contador", contador);
+        }
+        return mv;
+    }
+
     @RequestMapping("fotoMostrar")
-    public String fotoMostrar(@RequestParam("codigoUsuario") String codigoUsuario, Model modelo){
-        UsuarioDTO usuario = usuarioService.getUsuario(codigoUsuario);
+    public String fotoMostrar(HttpServletRequest request, Model modelo) {
+        UsuarioDTO usuario = usuarioService.getUsuario(request.getParameter("codigoUsuario"));
         modelo.addAttribute("usuario", usuario);
+        String foto = "";
+        if (usuario.getFoto() != null && usuario.getFoto().length > 0) {
+            foto = Base64.getEncoder().encodeToString(usuario.getFoto());
+        }
+        modelo.addAttribute("foto64", foto);
         return "fotoUsuario";
     }
-    
+
     @RequestMapping("fotoGrabar")
-    public ModelAndView fotoGrabar(@RequestParam("archivo") CommonsMultipartFile archivo, @RequestParam("codigoUsuario") String codigoUsuario){
+    public ModelAndView fotoGrabar(@RequestParam("archivo") CommonsMultipartFile archivo,
+            @RequestParam("codigoUsuario") String codigoUsuario) {
         UsuarioDTO usuario = usuarioService.getUsuario(codigoUsuario);
         usuario.setFoto(archivo.getBytes());
         return new ModelAndView("usuarioLista", "lista", usuarioService.getListaUsuarios());
